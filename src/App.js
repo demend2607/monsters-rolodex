7edf68cfeee4e70d1aa55aad6094c5e1ac6959a9
Vyacheslav <finder2607011@gmail.com>
2023-07-11 09:21:18 +1000

    monsters roblox complete and clean

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import SearchBox from './components/search-box/SearchBox.component';
 import CardList from './components/card-list/CardList.component';
@@ -7,18 +7,44 @@ import './App.css';
 
 const App = () => {
 	const [searchField, setSearchField] = useState('');
+	const [title, setTitle] = useState('');
+	const [monsters, setMonsters] = useState([]);
+	const [filteredMonsters, setFilterdMonsters] = useState(monsters);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => setMonsters(users));
+	}, []);
+
+	useEffect(() => {
+		const newFilteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
+		});
+		setFilterdMonsters(newFilteredMonsters);
+	}, [monsters, searchField]);
+
 	const onSearchChange = (event) => {
 		const searchFieldString = event.target.value.toLocaleLowerCase();
 		setSearchField(searchFieldString);
 	};
+
+	const onTitleChange = (event) => {
+		const searchFieldString = event.target.value.toLocaleLowerCase();
+		setTitle(searchFieldString);
+	};
+
 	return (
 		<div className="App">
-			<h1 className="app-title"> Monsters Rolodex</h1>
+			<h1 className="app-title">{title}</h1>
+
 			<SearchBox onChangeHandler={onSearchChange} placeholder="search monsters" className="search-box" />
-			{/* <CardList monsters={filteredMonsters} /> */}
+			<SearchBox onChangeHandler={onTitleChange} placeholder="set title" className="title-search-box" />
+			<CardList monsters={filteredMonsters} />
 		</div>
 	);
 };
+
 // class App extends Component {
 // 	constructor() {
 // 		super();
@@ -28,15 +54,15 @@ const App = () => {
 // 		};
 // 	}
 
-// 	componentDidMount() {
-// 		fetch('https://jsonplaceholder.typicode.com/users')
-// 			.then((response) => response.json())
-// 			.then((users) =>
-// 				this.setState(() => {
-// 					return { monsters: users };
-// 				})
-// 			);
-// 	}
+// componentDidMount() {
+// 	fetch('https://jsonplaceholder.typicode.com/users')
+// 		.then((response) => response.json())
+// 		.then((users) =>
+// 			this.setState(() => {
+// 				return { monsters: users };
+// 			})
+// 		);
+// }
 
 // 	onSearchChange = (event) => {
 // 		const searchField = event.target.value.toLocaleLowerCase();
